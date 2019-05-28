3cc658e65d7fdca190b3b83ad2b31f71e8de471a
Jakub Tomczak <jaycobe9@gmail.com>
2019-05-29 00:03:58 +0200

    TestMain virker

--- src/cv/TestMain.java
+++ src/cv/TestMain.java
@@ -2,9 +2,9 @@ package cv;
 public class TestMain {
 
 	public static void main(String[] args) {
-		ObjectReader test = new ObjectReader("C:\\Users\\Jakub Tomczak\\Desktop\\New folder (2)\\one.jpg");
-		ObjectReader test1 = new ObjectReader("C:\\Users\\Jakub Tomczak\\Desktop\\New folder (2)\\two.jpg");
-		ObjectReader test2 = new ObjectReader("C:\\Users\\Jakub Tomczak\\Desktop\\New folder (2)\\three.jpg");
+		ObjectReader test = new ObjectReader("a.jpg");
+		ObjectReader test1 = new ObjectReader("b.jpg");
+		ObjectReader test2 = new ObjectReader("c.jpg");
 		Thread th = new Thread(test);
 		Thread th1 = new Thread(test1);
 		Thread th2 = new Thread(test2);
